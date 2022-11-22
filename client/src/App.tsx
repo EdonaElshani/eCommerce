fc469e192b9997da6cd7208a5686524a93e5fe70
EdonaElshani <edonaelshani31@gmail.com>
2022-11-22 20:20:13 +0100

    day 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+// const products = [
+//   {name: "apple watch", price: 50.00},
+//   {name: "mac pro", price: 1850.00},
+//   {name: "iphone", price: 950.00},
+// ]
 function App() {
+  const [products, setProducts] = useState([
+    {name: "apple watch", price: 50.00},
+    {name: "mac pro", price: 1850.00},
+    {name: "iphone", price: 950.00},
+  ]);
+
+  function addProduct()
+  {
+    setProducts([...products, {name: "Product 1", price: 150.00}])
+  }
+  function addProduct2()
+  {
+    setProducts(prevArray => [...prevArray, {name: "Product " + (prevArray.length),  price: 150.00}])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Re-Store</h1>
+       <ul>
+          {products.map((item, index) => (
+            <li key={index}>
+              Emri i produktit: {item.name}, Cmimi: {item.price}
+            </li>
+          ))}
+       </ul>
+       <div>
+        <button onClick={addProduct}>Add Product</button>
+        <button onClick={addProduct2}>Add Another Product</button>
+       </div>
     </div>
   );
 }
