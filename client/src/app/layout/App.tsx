75d55fc3129fa74431643852bfcc81c93c418391
EdonaElshani <edonaelshani31@gmail.com>
2022-11-28 23:36:29 +0100

    react router install

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../fatures/about/AbutPage";
 import Catalog from "../../fatures/catalog/catalog";
 import Header from "../../fatures/catalog/Header";
+import ProductDetails from "../../fatures/catalog/ProductDetails";
+import ContactPage from "../../fatures/contact/ContactPage";
+import HomePage from "../../fatures/home/HomePage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -11,18 +16,25 @@ function App() {
       palette: {
         mode: paletteType,
         background: {
-          default: paletteType === 'light'? '#eaeaea' : "#121212"
+          default: paletteType === 'light' ? '#eaeaea' : "#121212"
         }
       }
     })
-  function handleThemeChange() {    
-      setDarkMode(!darkMode);
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/catalog/:id' element={<ProductDetails />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
         <Catalog />
       </Container>
     </ThemeProvider>
---
+++ client/src/fatures/catalog/ProductDetails.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function ProductDetails() {
+    return (
+        <>
+            <Typography variant='h4'>
+                Product Details Page
+            </Typography>
+        </>
+    )
+}
