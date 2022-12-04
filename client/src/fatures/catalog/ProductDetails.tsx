59f9e96eaf4c630ff2be2fe1223584ed3d9dbceb
EdonaElshani <edonaelshani31@gmail.com>
2022-12-04 14:43:04 +0100

    product details

--- client/src/fatures/catalog/ProductDetails.tsx
+++ client/src/fatures/catalog/ProductDetails.tsx
@@ -1,10 +1,23 @@
 import { Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
+    const [product, setProduct]= useState<Product | null>(null)
+    const[loading, setLoading]=useState(true);
+    useEffect(()=>{
+        axios.get(`http://localhost:1196/api/Product/${1}`)
+        .then(response=>setProduct(response.data))
+        .catch(error=>console.log(error))
+        .finally(()=>setLoading(false))
+    },[])
+    if(loading) return <h3>Loading...</h3>
+    if (!product) return <h3>Product not found...</h3>
     return (
         <>
             <Typography variant='h4'>
-                Product Details Page
+                {product?.name}
             </Typography>
         </>
     )
